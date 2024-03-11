668197a61a5058bb75c862dd34816ea989bfbd8d
Sharon Yang <sharonyyang0@gmail.com>
2024-03-11 19:04:09 -0400

    Started formatting

---
+++ src/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="px-24 py-2">
+      <h1 className="text-6xl font-bold">MFA Play</h1>
+      <hr className="m-5" />
+    </header>
+  );
+}
