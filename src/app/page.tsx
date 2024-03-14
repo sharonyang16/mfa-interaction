59ed4e6f27d30645c72d4b3a047421c7b44c0022
Sharon Yang <sharonyyang0@gmail.com>
2024-03-14 09:58:07 -0400

    #4 #5 added description and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography, Button } from "@mui/material";
 import Image from "next/image";
 import MainPainting from "./images/Felice Casorati (Italian, 1883-1963) A Student, c.1910 Oil on plywood panel Museum of Fine Arts, Boston..jpg";
 
 export default function Home() {
+  const buttonStyle =
+    "text-3xl w-full bg-red-600 hover:bg-red-700 active:bg-red-700 mt-10 py-5";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24">
       <Grid container spacing={2} className="w-full">
@@ -15,7 +17,27 @@ export default function Home() {
           />
         </Grid>
         <Grid item xs={4}>
-          {" "}
+          <Typography variant="h5">
+            <strong>
+              <em>A Student</em>
+            </strong>{" "}
+            by <strong>Felice Casorati</strong>
+          </Typography>
+          <Typography variant="body2" fontStyle="italic" textAlign="right">
+            Around 1930
+          </Typography>
+          <Typography variant="body1" textAlign="justify">
+            Felice Casorati’s oil painting A Student depicts a pupil in a studio
+            setting. Holding a palette with a hand on their hip, this student
+            seems to be thinking or reflecting on something in front of them.
+            What do you think the student is looking at?
+          </Typography>
+          <Button className={buttonStyle} variant="contained">
+            Vote
+          </Button>
+          <Button className={buttonStyle} variant="contained">
+            Create Your Own
+          </Button>
         </Grid>
         <Grid item xs={4}>
           {" "}
