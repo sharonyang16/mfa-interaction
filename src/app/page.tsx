62beecd6fd936c7e3c67601bfd9f92c1a940926a
Sharon Yang <sharonyyang0@gmail.com>
2024-03-14 10:03:42 -0400

    #4 #5 styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,32 +16,35 @@ export default function Home() {
             alt={`Felice Casorati's "A Student"`}
           />
         </Grid>
-        <Grid item xs={4}>
-          <Typography variant="h5">
-            <strong>
-              <em>A Student</em>
-            </strong>{" "}
-            by <strong>Felice Casorati</strong>
-          </Typography>
-          <Typography variant="body2" fontStyle="italic" textAlign="right">
-            Around 1930
-          </Typography>
-          <Typography variant="body1" textAlign="justify">
-            Felice Casorati’s oil painting A Student depicts a pupil in a studio
-            setting. Holding a palette with a hand on their hip, this student
-            seems to be thinking or reflecting on something in front of them.
-            What do you think the student is looking at?
-          </Typography>
-          <Button className={buttonStyle} variant="contained">
-            Vote
-          </Button>
-          <Button className={buttonStyle} variant="contained">
-            Create Your Own
-          </Button>
+        <Grid item xs={4} className="flex flex-col justify-between">
+          <div>
+            <Typography variant="h5" className="text-3xl">
+              <strong>
+                <em>A Student</em>
+              </strong>{" "}
+              by <strong>Felice Casorati</strong>
+            </Typography>
+            <Typography variant="body2" fontStyle="italic" textAlign="right">
+              Around 1930
+            </Typography>
+            <Typography variant="body1" textAlign="justify">
+              Felice Casorati’s oil painting A Student depicts a pupil in a
+              studio setting. Holding a palette with a hand on their hip, this
+              student seems to be thinking or reflecting on something in front
+              of them. What do you think the student is looking at?
+            </Typography>
+          </div>
+
+          <div>
+            <Button className={buttonStyle} variant="contained">
+              Vote
+            </Button>
+            <Button className={buttonStyle} variant="contained">
+              Create Your Own
+            </Button>
+          </div>
         </Grid>
-        <Grid item xs={4}>
-          {" "}
-        </Grid>
+        <Grid item xs={4}></Grid>
       </Grid>
     </main>
   );
